25e6dbf3e9354533f622b6b52ffc615781f0f53d
TJ DeVries <devries.timothyj@gmail.com>
2024-02-29 16:43:06 -0500

    init: this works on my machine

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, FuzzySuggestModal, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
+import { resolve } from 'path';
 
 // Remember to rename these classes and interfaces!
 
@@ -10,77 +11,65 @@ const DEFAULT_SETTINGS: MyPluginSettings = {
 	mySetting: 'default'
 }
 
+const REFRESHED_TIMES: Map<string, number> = new Map();
+const FILE_START_FLAG = "%% START CODE:";
+const FILE_END_FLAG = "%% END CODE %%";
+
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		// TODO: Would be good to double check all the times on startup, but who cares LUL
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		this.registerEvent(
+			this.app.vault.on("modify", async (file: TFile) => {
+				const text = await this.app.vault.cachedRead(file);
+				const lines: string[] = text.split("\n");
+				for (let i = 0; i < lines.length; i++) {
+					const line = lines[i];
+					if (line.includes(FILE_START_FLAG)) {
+						const offset = lines.slice(i).indexOf(FILE_END_FLAG)!;
+
+						let name = line.substring(FILE_START_FLAG.length, line.length - 3).trim();
+						name = name.substring(2, name.length - 2);
+
+						const resolved = this.app.metadataCache.getFirstLinkpathDest(name, '');
+						const last_refreshed = REFRESHED_TIMES.get(name) || 0;
+						if (resolved && last_refreshed < resolved.stat.mtime) {
+							// Update the refreshed times
+							REFRESHED_TIMES.set(name, resolved.stat.mtime);
+
+							let contents = await this.app.vault.cachedRead(resolved);
+							const new_lines = [
+								"```" + resolved.extension,
+								contents.trim(),
+								"```",
+								FILE_END_FLAG
+							];
+							lines.splice(i + 1, offset, ...new_lines);
+							this.app.vault.modify(file, lines.join("\n"));
+						}
+					}
+				}
+			})
+		);
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: 'add-code-include',
+			name: 'Add a CODE INCLUDE block',
 			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+				new CodeIncluderModal(this.app).open();
 			}
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		// this.addSettingTab(new SampleSettingTab(this.app, this));
 	}
 
-	onunload() {
-
-	}
+	onunload() { }
 
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
@@ -91,19 +80,20 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+class CodeIncluderModal extends FuzzySuggestModal<TFile> {
+	getItems(): TFile[] {
+		return this.app.vault.getFiles()
 	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+	/**
+	 * @public
+	 */
+	getItemText(item: TFile): string { return item.name }
+	/**
+	 * @public
+	 */
+	onChooseItem(item: TFile, _evt: MouseEvent | KeyboardEvent): void {
+		const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+		view?.editor.replaceSelection([FILE_START_FLAG + " [[" + item.path + "]] %%", FILE_END_FLAG].join("\n"));
 	}
 }
 
@@ -116,7 +106,7 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
